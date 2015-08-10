9da15a885c85090ddf7f9c2414b2a759a04aa969
Diogo Rolo <diogo.rolo@outlook.com>
2015-08-10 10:17:26 +0100

    Enhances isPrime function

--- common.py
+++ common.py
@@ -1,9 +1,22 @@
 from math import sqrt,ceil
 
 def isPrime(num):
-	for i in range(2,int(ceil(sqrt(num)))+1):
-		if num % i == 0 and i != num:
+	if num == 1: # 1 is not prime
+		return False
+	if num < 4: # 2 and 3 are prime
+		return True
+	if num % 2 == 0: # even numbers bigger than 2 are not prime
+		return False
+	if num % 3 == 0: # if the number is divisible by 3 is not prime
+		return False
+	f = 5 # we've already test up to #4
+	r = int(ceil(sqrt(num))) # a prime number can be checked by trial test up to sqrt(n)
+	while f <= r:
+		if num % f == 0:
 			return False
+		if num % (f+2) == 0:
+			return False
+		f += 6 # a prime is always is the form 6k +- 1
 	return True
 	
 	
